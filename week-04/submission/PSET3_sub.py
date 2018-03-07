f62741b495b61d709d2cd8865ea4469f39aaa132
Charlotte Ong <cxyong@Charlottes-MacBook-Pro.local>
2018-03-07 00:27:43 -0500

    latest

--- week-04/submission/PSET3_sub.py
+++ week-04/submission/PSET3_sub.py
@@ -4,3 +4,104 @@ import pandas as pd
 
 import os
 os.chdir('week-04')
+from twitter_keys import api_key, api_secret
+
+auth = tweepy.AppAuthHandler(api_key, api_secret)
+# wait_on_rate_limit and wait_on_rate_limit_notify are options that tell our API object to automatically wait before passing additional queries if we come up against Twitter's wait limits (and to inform us when it's doing so).
+api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+
+def auth(key, secret):
+  auth = tweepy.AppAuthHandler(key, secret)
+  api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+  # Print error and exit if there is an authentication error
+  if (not api):
+      print ("Can't Authenticate")
+      sys.exit(-1)
+  else:
+      return api
+
+api = auth(api_key, api_secret)
+
+def get_tweets(
+    geo,
+    out_file,
+    search_term = '',
+    tweet_per_query = 100,
+    tweet_max = 150,
+    since_id = None,
+    max_id = -1,
+    write = False
+  ):
+  tweet_count = 0
+  # all_tweets = pd.DataFrame()
+  while tweet_count < tweet_max:
+    try:
+      if (max_id <= 0):
+        if (not since_id):
+          new_tweets = api.search(
+            q = search_term,
+            rpp = tweet_per_query,
+            geocode = geo
+          )
+        else:
+          new_tweets = api.search(
+            q = search_term,
+            rpp = tweet_per_query,
+            geocode = geo,
+            since_id = since_id
+          )
+      else:
+        if (not since_id):
+          new_tweets = api.search(
+            q = search_term,
+            rpp = tweet_per_query,
+            geocode = geo,
+            max_id = str(max_id - 1)
+          )
+        else:
+          new_tweets = api.search(
+            q = search_term,
+            rpp = tweet_per_query,
+            geocode = geo,
+            max_id = str(max_id - 1),
+            since_id = since_id
+          )
+      if (not new_tweets):
+        print("No more tweets found")
+        break
+      for tweet in new_tweets:
+        # all_tweets = all_tweets.append(parse_tweet(tweet), ignore_index = True)
+        if write == True:
+            with open(out_file, 'w') as f:
+                f.write(jsonpickle.encode(tweet._json, unpicklable=False) + '\n')
+      max_id = new_tweets[-1].id
+      ## return the last thing in the array
+      tweet_count += len(new_tweets)
+      ## adds the number of new tweets
+      ## += is python shorthand for tweet_count = tweet_count + len(new_tweets)
+
+    except tweepy.TweepError as e:
+      # Just exit if any error
+      print("Error : " + str(e))
+      break
+  print (f"Downloaded {tweet_count} tweets.")
+  # return all_tweets
+
+# Set a Lat Lon
+latlng = '42.359416,-71.093993' # Eric's office (ish)
+# Set a search distance
+radius = '5mi'
+# See tweepy API reference for format specifications
+geocode_query = latlng + ',' + radius
+# set output file location
+file_name = 'data/tweets.json'
+# set threshold number of Tweets. Note that it's possible
+# to get more than one
+t_max = 2000
+
+get_tweets(
+  geo = geocode_query,
+  tweet_max = t_max,
+  write = True,
+  out_file = file_name
+)
